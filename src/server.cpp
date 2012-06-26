f78736402cdf6ae858cb2461f06ad3bc2231caa3
0110 <github@buxs.org>
2012-06-26 20:30:33 +0200

    Small workaround to compile the serverpart with XCode

--- src/server.cpp
+++ src/server.cpp
@@ -3,7 +3,13 @@
 // ~~~~~~~~~~
 //
 
+// This is a small workaround to get the source easily compiled with XCode.
+// to get this work, the following constant has to be defined. I assume it not present with CMake
+#ifdef __XCODE_USED__ 
+#include "server_tcpconnection.hpp"
+#else
 #include "libfullcircle/server_tcpconnection.hpp"
+#endif
 
 using namespace fullcircle;
 
