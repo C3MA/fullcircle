1902fe730af8579921ec3400172cdfbd0fd1cfdd
0110 <github@buxs.org>
2012-07-18 23:50:53 +0200

    Generated a dummy version for fc-add out of fc-text

---
+++ src/fc-add.cpp
@@ -0,0 +1,92 @@
+#include <libfullcircle/common.hpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <QApplication>
+#include <boost/filesystem.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/lexical_cast.hpp>
+#include <libfullcircle/sequence.hpp>
+#include <libfullcircle/frame.hpp>
+#include <libfullcircle/sprite_io.hpp>
+#include <libfullcircle/fontrenderer.hpp>
+#include <libfullcircle/perlin_noise.hpp>
+#include <libfullcircle/color_scheme_smash.hpp>
+#include <libfullcircle/sfx/frame_fader.hpp>
+#include <boost/program_options.hpp>
+#include <boost/program_options/positional_options.hpp>
+namespace po = boost::program_options;
+namespace bfs=boost::filesystem;
+
+int main (int argc, char* argv[]) {
+
+  /*** 
+   * Careful! This must be in the top level namespace of any binary, and must
+   * be called only once. Just leave it here.
+   */
+  Q_INIT_RESOURCE(sprites);
+	
+  /* Set the default forgroundcolor to white */
+	
+  try {
+    std::ostringstream oss;
+    oss << "Usage: " << argv[0] << " ACTION [additional options]";
+    po::options_description desc(oss.str());
+    desc.add_options()
+      ("help,h", "produce help message")
+      ("version,v", "print version and exit")
+      ("sequence,s", po::value<std::string>(), "the sequence file to use")
+      ("text,t", po::value<std::string>(), "the text to display")
+      ("moving,m", po::value<std::string>(), "time one character should be displayed before moving to the next")
+	  ("color,c", po::value<std::string>(), "text color in the format: '#RRGGBB' RR is red, GG green, BB blue (range of each value is 0x00-0xFF)")
+     ;
+    po::positional_options_description p;
+    p.add("sequence", 1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+        options(desc).positional(p).run(), vm);
+    po::notify(vm);
+
+    // Begin processing of commandline parameters.
+    std::string sequencefile;
+    std::string text;
+    uint32_t scrolltime;
+
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 1;
+    }
+
+    fullcircle::VersionInfo::Ptr version(new fullcircle::VersionInfo());
+    if (vm.count("version")) {
+      std::cout << "fullcircle version " << version->getVersion() << std::endl;
+      return 0;
+    }
+
+    if (vm.count("sequence") != 1 ) {
+      std::cerr << "You must specify a sequence file (-s <filename>)." << std::endl;
+      return 1;
+    } else {
+      sequencefile=vm["sequence"].as<std::string>();
+    }
+
+
+    bfs::path sequence(sequencefile);
+	  
+    try {
+	  fullcircle::Sequence::Ptr seq(new fullcircle::Sequence(25,8,8));
+
+    } catch (fullcircle::GenericException& ex) {
+      std::cout << "Caught exception: " << ex.what() << std::endl;
+      exit(1);
+    }
+  } catch(std::exception& e) {
+    std::cerr << "error: " << e.what() << std::endl;
+    return 1;
+  } catch(...) {
+    std::cerr << "Exception of unknown type!" << std::endl;
+    return 1;
+  }
+  return 0;
+}
